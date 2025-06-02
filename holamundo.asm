711b606229fa47cd5152851d411edb6b06b561e9
Lucho <lucncd@gmail.com>
2025-06-02 12:15:02 -0500

    feat: first commit

---
+++ holamundo.asm
@@ -0,0 +1,21 @@
+global _start
+
+section .data
+    message db "Hola Mundo", 0xA
+    message_length equ $ - message
+
+section .text
+
+_start:
+    ; print 'Hola Mundo'
+    mov rax, 1
+    mov rdi, 1
+    mov rsi, message
+    mov rdx, message_length
+    syscall
+
+    ; syscall exit
+
+    mov rax, 0x3C
+    mov rdi, 0
+    syscall
